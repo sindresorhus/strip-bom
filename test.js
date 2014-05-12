535ee9dd996e429e766dee48e68c09f12fabe99c
Mathias Bynens <mathias@qiwi.be>
2014-05-12 12:12:31 +0200

    Test non-UTF-8-encoded files that contain EF BB BF
    Byte sequences that look like a UTF-8-encoded BOM must only be stripped from UTF-8-encoded files. This patch makes sure we actually test if stripping non-UTF-8 files is successfully prevented.

--- test.js
+++ test.js
@@ -8,12 +8,12 @@ it('should strip BOM from UTF-8 string/buffer', function () {
 	assert.strictEqual(stripBom(fs.readFileSync('fixture-utf8')).toString(), 'Unicorn\n');
 });
 
-it('should not strip BOM from UTF16LE', function () {
+it('should not strip anything that looks like a UTF-8-encoded BOM from UTF16LE', function () {
 	var f = fs.readFileSync('fixture-utf16le');
 	assert.strictEqual(stripBom(f), f);
 });
 
-it('should not strip BOM from UTF16BE', function () {
+it('should not strip anything that looks like a UTF-8-encoded BOM from UTF16BE', function () {
 	var f = fs.readFileSync('fixture-utf16be');
 	assert.strictEqual(stripBom(f), f);
 });
