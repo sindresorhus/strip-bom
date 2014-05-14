6c78de1e101e62e27aedacb4e1139d3243bf0444
Sindre Sorhus <sindresorhus@gmail.com>
2014-05-14 23:38:50 +0200

    add test for #2

--- test.js
+++ test.js
@@ -9,6 +9,11 @@ it('should strip BOM from UTF-8 string/buffer', function () {
 	assert.strictEqual(stripBom(fs.readFileSync('fixture-utf8')).toString(), 'Unicorn\n');
 });
 
+it('should only strip BOM from the beginning of a file', function () {
+	var f = fs.readFileSync('fixture-utf8-bom-middle', 'utf8');
+	assert.strictEqual(stripBom(f), f);
+});
+
 it('should not strip anything that looks like a UTF-8-encoded BOM from UTF16LE', function () {
 	var f = fs.readFileSync('fixture-utf16le');
 	assert.strictEqual(stripBom(f), f);
