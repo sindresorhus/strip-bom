068d55a626ab8c468acc798f57f3eb9e7736dfc9
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-30 22:46:23 +0700

    [BREAKING] Extract buffer support into a separate module and require Node.js >=4

--- test/test.js
+++ test/test.js
@@ -1,26 +1,12 @@
-'use strict';
-var assert = require('assert');
-var fs = require('fs');
-var stripBom = require('../');
+import fs from 'fs';
+import test from 'ava';
+import m from '../';
 
-process.chdir(__dirname);
-
-it('should strip BOM from UTF-8 string/buffer', function () {
-	assert.strictEqual(stripBom(fs.readFileSync('fixture-utf8', 'utf8')), 'Unicorn\n');
-	assert.strictEqual(stripBom(fs.readFileSync('fixture-utf8')).toString(), 'Unicorn\n');
+test('strips BOM from string', t => {
+	t.is(m(fs.readFileSync('fixture-utf8', 'utf8')), 'Unicorn\n');
 });
 
-it('should only strip BOM from the beginning of a file', function () {
-	var f = fs.readFileSync('fixture-utf8-bom-middle', 'utf8');
-	assert.strictEqual(stripBom(f), f);
-});
-
-it('should not strip anything that looks like a UTF-8-encoded BOM from UTF16LE', function () {
-	var f = fs.readFileSync('fixture-utf16le');
-	assert.strictEqual(stripBom(f), f);
-});
-
-it('should not strip anything that looks like a UTF-8-encoded BOM from UTF16BE', function () {
-	var f = fs.readFileSync('fixture-utf16be');
-	assert.strictEqual(stripBom(f), f);
+test('strips BOM from the beginning of a file', t => {
+	const f = fs.readFileSync('fixture-utf8-bom-middle', 'utf8');
+	t.is(m(f), f);
 });
